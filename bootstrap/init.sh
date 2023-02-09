3eaa0dce0bf1c2ba1f68c0608e2812ad5edd483c
Pintu <plohar@sk.com>
2023-02-09 20:07:23 +0530

    directory changes experimental

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Function to check the pod status
+
+
+
 # Create repository and API Token variables
 repository=https://github.com/pintulohargcetts/argocd-application-example.git
 echo "using $repository to setup your apps"
+password=calltech
 #read -p "Enter the full repository URL (with .git at the end): " repository
 read -p "Enter the Git API token with api scope permissions: " git_api_token
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
@@ -22,7 +27,8 @@ helm install argocd -n argocd argo/argo-cd -f values.yaml
 echo "Configuring repository access..."
 kubectl create secret generic example-infrastructure-repo --namespace=argocd --type=Opaque --from-literal=url=$repository --from-literal=password=$git_api_token
 kubectl label secret -n argocd example-infrastructure-repo argocd.argoproj.io/secret-type=repository
-
+./scripts/check_pods_status.sh
+echo $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 # Create project
 echo "Creating the main project..."
 kubectl apply -f manifests/project.yaml
---
+++ bootstrap/scripts/init_credentials.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+argoPass=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo $argoPass
+echo "Loggin in with randomly generated password"
+
+argocd login --insecure --grpc-web localhost:8080 --username admin --password $argoPass
+
+echo "setting the credentials"
+argocd account update-password --account admin --current-password  $argoPass --new-password 'test'
+
+echo "Now you can login with user:admin & password :test"
+argocd login --insecure --grpc-web localhost:8080 --username admin --password 
